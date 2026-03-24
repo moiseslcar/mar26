b18ef8840220642ba47422aba8a1d827345a1cd1
Moises Carvalho <moiseslcar@gmail.com>
2026-03-24 14:41:14 -0300

    Ajsutando main.tf p Privisonar instancia exemplo

--- main.tf
+++ main.tf
@@ -9,6 +9,15 @@ terraform {
 }
 
 provider "aws" {
-    region = us-east-1
+    region = "us-east-1"
     profile = "bia"
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-02f3f602d23f1659d"
+    instance_type = "t3.micro"
+    tags = {
+        Name = "Exemplo"
+        ambiente = "dev"
+    }
 }
