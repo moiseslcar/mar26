46703bc2e22f4d0c3845f1c998c52b03c3db6ac4
Moises Carvalho <moiseslcar@gmail.com>
2026-03-24 15:48:35 -0300

    Alterando SG

--- main.tf
+++ main.tf
@@ -17,7 +17,11 @@ resource "aws_instance" "example" {
     ami           = "ami-02f3f602d23f1659d"
     instance_type = "t3.micro"
     tags = {
-        Name = "Exemplo"
+        Name = "bia-local"
         ambiente = "dev"
+    }
+    vpc_security_group_ids = ["sg-0308d881340c665a5"]
+    root_block_device {
+      volume_size = 10
     }
 }
