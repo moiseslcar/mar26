d741170b78e87838c761c7ea8cf492e9c260d073
Moises Carvalho <moiseslcar@gmail.com>
2026-04-06 16:07:46 -0300

    Adicionando subnets a e b no locals e cluster ecs

--- bia-tf/locals.tf
+++ bia-tf/locals.tf
@@ -2,6 +2,18 @@ data "aws_vpc" "default" {
   default = true
 }
 
+data "aws_subnet" "zona_a" {
+  vpc_id            = data.aws_vpc.default.id
+  availability_zone = "us-east-1a"
+}
+
+data "aws_subnet" "zona_b" {
+  vpc_id            = data.aws_vpc.default.id
+  availability_zone = "us-east-1b"
+}
+
 locals {
-  vpc_id = data.aws_vpc.default.id
+  vpc_id        = data.aws_vpc.default.id
+  subnet_zona_a = data.aws_subnet.zona_a.id
+  subnet_zona_b = data.aws_subnet.zona_b.id
 }
