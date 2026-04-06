d6e61a6107fad97e74279491bb0146e44ad188e5
Moises Carvalho <moiseslcar@gmail.com>
2026-04-06 17:06:34 -0300

    Adic. ECR, CWLogGoup, CapProvider e tasfDef

---
+++ bia-tf/aws_task_definition_bia_web.tf
@@ -0,0 +1,25 @@
+resource "aws_ecs_task_definition" "bia-web" {
+  family       = "task-def-bia"
+  network_mode = "bridge"
+
+  container_definitions = jsonencode([{
+    name              = "bia",
+    image             = "${aws_ecr_repository.bia.repository_url}:latest",
+    essential         = true
+    portMappings      = [{ containerPort = 8080, hostPort = 80 }],
+    cpu               = 1024
+    memoryReservation = 400
+    environment = [
+      { name = "DB_PORT", value = "5432" },
+      { name = "DB_HOST", value = "${aws_db_instance.bia.address}" }
+    ]
+    logConfiguration = {
+      logDriver = "awslogs",
+      options = {
+        "awslogs-region"        = "us-east-1",
+        "awslogs-group"         = aws_cloudwatch_log_group.ecs_bia_web.name,
+        "awslogs-stream-prefix" = "bia"
+      }
+    },
+  }])
+}
--- bia-tf/outputs.tf
+++ bia-tf/outputs.tf
@@ -27,3 +27,7 @@ output "rds_secrets" {
   description = "Secrets associado ao RDS"
   value       = tolist(aws_db_instance.bia.master_user_secret)[0].secret_arn
 }
+
+output "bia_repo_url" {
+  value = aws_ecr_repository.bia.repository_url
+}
