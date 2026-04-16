e2743554a48a219be9fcab581cdf5f45668b6383
Moises Carvalho <moiseslcar@gmail.com>
2026-04-16 18:02:49 -0300

    Bia com ALB, 2 tasks e persistindo no RDS

--- bia-tf/aws_ecs_service_bia_web.tf
+++ bia-tf/aws_ecs_service_bia_web.tf
@@ -2,7 +2,7 @@ resource "aws_ecs_service" "bia" {
   name            = "service-bia"
   cluster         = aws_ecs_cluster.cluster-bia.id
   task_definition = aws_ecs_task_definition.bia-web.arn
-  desired_count   = 1
+  desired_count   = 2
 
   capacity_provider_strategy {
     capacity_provider = aws_ecs_capacity_provider.bia.name
@@ -14,12 +14,12 @@ resource "aws_ecs_service" "bia" {
     type  = "spread"
     field = "attribute:ecs.availability-zone"
   }
-  deployment_minimum_healthy_percent = 0
+  deployment_minimum_healthy_percent = 50
   deployment_maximum_percent         = 100
 
-  lifecycle {
-    ignore_changes = [desired_count]
-  }
+  #lifecycle {
+  #  ignore_changes = [desired_count]
+  #}
 
   depends_on = [aws_lb_target_group.tg-bia]
 
